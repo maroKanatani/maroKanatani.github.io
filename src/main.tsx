7914adf0c80f7718aab943974fc8a98c0592a99f
maroKanatani <16130443+maroKanatani@users.noreply.github.com>
2024-09-23 21:51:46 +0900

    chore: setup biome

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { render } from 'preact'
 import { App } from './app.tsx'
 import './index.css'
 
-render(<App />, document.getElementById('app')!)
+// biome-ignore lint/style/noNonNullAssertion: This is a root file, so it's okay to use non-null assertion here.
+render(<App />, document.getElementById("app")!);
